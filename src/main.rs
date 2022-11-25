3ca7b86146f2c6d127b421058acd5694f6f6e933
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2022-11-25 17:19:45 -0600

    test palette

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use rs_ws281x::{ChannelBuilder, StripType, ControllerBuilder};
 use tokio;
-use color::Hsv;
+use palette;
 
 fn main() {
-    let led_count = std::env::args().nth(1).expect("no led count").parse().unwrap_or(10);
-    let color_r = std::env::args().nth(2).expect("no r").parse().unwrap_or(10u8);
-    let color_g = std::env::args().nth(3).expect("no g").parse().unwrap_or(10u8);
-    let color_b = std::env::args().nth(4).expect("no b").parse().unwrap_or(10u8);
+    let mut led_count = std::env::args().nth(1).unwrap_or("-1".to_string()).parse().expect("Failed to parse number of leds!");
+
+    if led_count == -1 {
+        println!("Warning: Missing led count, defaulting to 10.");
+        led_count = 10;
+    }
+
     let mut controller = ControllerBuilder::new()
         .freq(800_000)
         .dma(10)
@@ -29,6 +32,9 @@ fn main() {
         hue = hue + 0.01;
         if hue > 1.0 { hue = 0.0; }
 
-        controller.render().unwrap();   
+        let color_rgba = palette::RgbHue::from_degrees(hue);
+        println!("{:?}", color_rgba);
+
+        controller.render().unwrap();
     }
 }
